04bca93cc2c62b8d1b3e7ec10c2d0b6abf838414
Ash McKenzie <ash@the-rebellion.net>
2015-08-17 18:43:00 +1000

    Tweaked timing

--- haproxy/config/reload_haproxy.sh
+++ haproxy/config/reload_haproxy.sh
@@ -7,7 +7,7 @@ current_pid=$(pgrep -n -f '/usr/sbin/haproxy')
 iptables -I INPUT -p tcp --dport 80 --syn -j DROP
 sleep 1
 /usr/sbin/haproxy -V -db -f /etc/haproxy/haproxy.cfg -st ${current_pid} &
-sleep 3
+sleep 1
 iptables -D INPUT -p tcp --dport 80 --syn -j DROP
 
 echo "INFO: Reloaded HAProxy"
