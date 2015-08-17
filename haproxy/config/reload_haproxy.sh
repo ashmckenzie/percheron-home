1cd22d687a91ca884b1529b074e5d3583fab4b21
Ash McKenzie <ash@the-rebellion.net>
2015-08-17 18:35:51 +1000

    nice reload!

---
+++ haproxy/config/reload_haproxy.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+echo "INFO: Reloading HAProxy"
+
+current_pid=$(pgrep -n -f '/usr/sbin/haproxy')
+
+iptables -I INPUT -p tcp --dport 80 --syn -j DROP
+sleep 1
+/usr/sbin/haproxy -V -db -f /etc/haproxy/haproxy.cfg -st ${current_pid} &
+sleep 3
+iptables -D INPUT -p tcp --dport 80 --syn -j DROP
+
+echo "INFO: Reloaded HAProxy"
